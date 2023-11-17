80021963536c6eba31151bdef84f9ba7993dc994
MAJDOUB <abdelkarim.majdoub@capgemini.com>
2023-11-17 18:28:23 +0100

    Creation of the application interface

---
+++ App/MainWindow.py
@@ -0,0 +1,15 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.uic import loadUi
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        loadUi("./app.ui", self)
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    sys.exit(app.exec_())
