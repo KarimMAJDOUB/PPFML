a13840c98fbeef32acf6cb30c981560abeed8ad9
unknown <ousslarkem@gmail.com>
2023-12-22 15:48:44 +0100

    Split and Model

--- scripts/Split_Test_Model.py
+++ scripts/Split_Test_Model.py
@@ -53,7 +53,6 @@ def Train_and_evaluate(model_name, X_train, X_test, y_train, y_test, index):
     y_pred = model.predict(X_test)
 
     mse = mean_squared_error(y_test, y_pred)
-    r2 = r2_score(y_test, y_pred)
     score = model.score(X_test, y_test)
 
     plt.figure()
